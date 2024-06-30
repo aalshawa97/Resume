500c6e8c7a342dcf199345ff48a44fd6f750b6ef
Abdullah Mutaz Alshawa <abdullahalshawa@gmail.com>
2024-06-30 16:42:31 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,10 @@ export default function App() {
           Summary: A software engineer with a passion for building innovative solutions. I have experience in website development, cyber security, etc. Seeking opportunities in engineering, IT, etc. My passion is helping others and I feel that technology is a platform to give back to society! Please reach out to me. Thank you!
         </p>
         <p>
-          Experience: As Computer Science teacher I teach enjoy teaching object web safety, cybersecurity, oriented programming, etc. Languages I use for students may include Scratch, Python, Java, HTML, CSS, JavaScript, etc. I also may assist with teaching realted courses for Math and Science. 
+          Job: As Computer Science teacher I teach enjoy teaching object web safety, cybersecurity, oriented programming, etc. Languages I use for students may include Scratch, Python, Java, HTML, CSS, JavaScript, etc. I also may assist with teaching realted courses for Math and Science. 
+        </p>
+        <p>
+          Industry Experience: As a Chrome OS Compatibility Testing Engineer at Samson Software Corporation, I have been afforded the unique opportunity to spearhead a project focused on bug triage. This project has allowed me to drastically expand my predictive analytics and behavior analysis capabilities. Through my experience, the project has produced substantial profitable results for Google. Chrome OS testing for end client Google LLC through HCL. Bug triage for PlayStore applications. IT imaging machines and running Python scripts for testing. Setting up and configuring desktop, mobile, ethernet devices, and server platforms. 
         </p>
         <p>
           Education: BSSE in computer software engineering technology with a minor in electrical engineering and renewable energy engineering from the Oregon Institute of Technology. 
